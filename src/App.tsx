9e4ae21f60ba9083648879e5c08cea58c045333c
solanke <solomonhizkiel@gmail.com>
2025-08-16 11:37:48 +0300

    install chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Button, ButtonGroup } from "@chakra-ui/react"
+
+function App() {
+  return <Button colorScheme="blue">Blue</Button>
+}
+  
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Provider } from './components/ui/provider.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+      
+    </Provider>
+  </React.StrictMode>,
+)
