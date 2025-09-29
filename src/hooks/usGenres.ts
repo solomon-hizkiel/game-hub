01ba494d16a6ac3b6e30e3431236a6e61b59d152
solanke <solomonhizkiel@gmail.com>
2025-09-29 10:03:37 +0300

    ship genres with the app

--- src/hooks/usGenres.ts
+++ src/hooks/usGenres.ts
@@ -1,14 +1,9 @@
-import { useEffect, useState } from "react";
-import type { Game } from "./useGames";
-import apiClient from "@/services/api-client";
-import { CanceledError } from "axios";
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface Genre{
   id:number;
   name:string;
   image_background:string;
 }
 
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({data:genres, isLoading:false,error:null})
 export default useGenres;
